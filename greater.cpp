3d43d8b8bad26ba257daa3eaf8284ce9a446781e
danquaheric <108493273+danquaheric@users.noreply.github.com>
2022-10-09 08:02:59 +0000

    Add files via upload

---
+++ greater.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int a, b;
+    cout<<"Enter 2 numbers: ";
+    cin>>a>>b;
+    if(a>b)
+        cout<<"greater ="<<a;
+    else
+        cout<<"greater ="<<b;
+    return 0;
+}
